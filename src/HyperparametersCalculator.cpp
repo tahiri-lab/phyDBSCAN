39d73a35aff7a03d812b5542dd316751202ee5f5
Thibaut <thibaut.leval@ecole.ensicaen.fr>
2023-08-24 16:31:41 -0400

    Taken into account "0" when calculating the best epsilon, changes in conditions

--- src/HyperparametersCalculator.cpp
+++ src/HyperparametersCalculator.cpp
@@ -8,28 +8,37 @@ double findBestEpsilon(const std::vector<Point>& points) {
         distanceArray.push_back(point.distances);
     }
 
-    std::vector<double> maxDiffs;
+    std::vector<double> numBeforeIncreaseList;
     for (const std::vector<double>& distances : distanceArray) {
         std::vector<double> sortedDistances = distances;
         std::sort(sortedDistances.begin(), sortedDistances.end());
 
         double maxDiff = 0.0;
-        for (size_t i = 1; i < sortedDistances.size(); ++i) {
-            double diff = sortedDistances[i] - sortedDistances[i - 1];
-            if (diff > maxDiff) {
-                maxDiff = diff;
-            }
+        double numBeforeIncrease = 0.0;
+        // We start at 2 because we don't want to compare the point with itself (the first element of the array)
+        for (size_t i = 2; i < sortedDistances.size(); ++i) {
+                double diff = sortedDistances[i] - sortedDistances[i - 1];
+                // Check if the difference is larger than the previous maximum and if the previous distance is 0
+                // If the previous distance is 0, it means that we use an average to avoid distorting the search for the epsilon
+                if (diff > maxDiff && sortedDistances[i - 1] == 0) {
+                    maxDiff = diff;
+                    numBeforeIncrease = (sortedDistances[i] + sortedDistances[i - 1]) / 2;
+                }
+                // Check if the difference is larger than the previous maximum and if the previous distance is not 0
+                else if (diff > maxDiff) {
+                    maxDiff = diff;
+                    numBeforeIncrease = sortedDistances[i - 1];
+                }
         }
-
-        maxDiffs.push_back(maxDiff);
+        numBeforeIncreaseList.push_back(numBeforeIncrease);
     }
 
-    double totalMaxDiff = 0.0;
-    for (double diff : maxDiffs) {
-        totalMaxDiff += diff;
+    double totalNumBeforeIncrease = 0.0;
+    for (double numBeforeIncrease : numBeforeIncreaseList) {
+        totalNumBeforeIncrease += numBeforeIncrease;
     }
 
-    return totalMaxDiff / maxDiffs.size();
+    return totalNumBeforeIncrease / numBeforeIncreaseList.size();
 }
 
 int findBestMinPoints(const std::vector<Point>& points, double epsilon) {
