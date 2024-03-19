881db53b318851c5e952c289a02e01b86c933e91
annartiges <anna.artiges@yahoo.fr>
2024-03-19 14:38:45 -0400

    Time and ARI added to output

--- src/poc.cpp
+++ src/poc.cpp
@@ -5,17 +5,45 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <chrono>
 
-//using namespace std;
+using namespace std;
+
+
+void timeToInt(long& temps, auto time)
+{
+    auto µs_time = std::chrono::time_point_cast<std::chrono::microseconds>(time);
+    auto epoch_time = µs_time.time_since_epoch();
+    auto value_time = std::chrono::duration_cast<std::chrono::microseconds>(epoch_time);
+    temps = value_time.count();
+}
+
+void computation(std::vector<Point>& points, std::string& Partition,  std::map <std::string, int>& clusterParam, double& ARI, double& bestEpsilon, int& bestMinPoints)
+{
+    bestEpsilon = findBestEpsilon(points);
+    //std::cout << "> Best epsilon: " << bestEpsilon << std::endl;
+    bestMinPoints = findBestMinPoints(points, bestEpsilon);
+    //std::cout << "> Best minPoints: " << bestMinPoints << std::endl;
+
+    determineCoreAndGroup(points, bestEpsilon, bestMinPoints);
+    determineBorderAndOutlier(points, bestEpsilon);
+    printResults(points, Partition);
+
+    if (canCalculateARI(points)) {
+        int fileDefinedGroupCount = clusterParam["#Clusters"];
+        ARI = calculateARI(points, fileDefinedGroupCount, clusterParam["#Trees"]);
+    }
+}
+
 
 int main(int nargc, char **argv) {
+    using std::chrono::system_clock;
 
     if(nargc < 3) {
         printf("\nWrong command format. Format must be :\n\t > ./phyDBSCAN input_file output_file.csv \n");
         exit(1);
         }
-
-
+    
     std::vector<Point> points;
     std::map <std::string, int> infoCSV;
     std::string fileName = argv[1];
@@ -23,12 +51,13 @@ int main(int nargc, char **argv) {
     std::ifstream inputFile(fileName);
     std::string line;
     std::string partition;
-    //char * partition = (char*)malloc(1000*sizeof(char));
-    int x = 0;
-
+    double best_Epsilon;
+    int best_MinPoints;
+    long start;
+    long end;
+    auto starting = std::chrono::system_clock::now();
 
     std::cout << "\n==== DBSCAN Clustering Algorithm ====\n" << std::endl;
-    //readInputFile("resources/input_data.txt", points, infoCSV);
     std::cout << "Reading input data..." << std::endl;
     int i = 0;
 
@@ -38,7 +67,6 @@ int main(int nargc, char **argv) {
             std::istringstream lineStream(line);
             Point point;
             double distance;
-
             while (lineStream >> distance) {
                 point.distances.push_back(distance);
             }
@@ -48,6 +76,9 @@ int main(int nargc, char **argv) {
         
         else if (line[0] >= 49 && line[1]>=48)
         {
+            // Initialize the starting time here
+            // Here that we start parsing and retrieving the necessary data.
+            starting = std::chrono::system_clock::now();
             std::istringstream lineStream(line);
             int data;
 
@@ -62,61 +93,36 @@ int main(int nargc, char **argv) {
             i = 0;
         }
 
-        else if (line[0] == 0) {
-
-            double bestEpsilon = findBestEpsilon(points);
-            std::cout << "> Best epsilon: " << bestEpsilon << std::endl;
-            int bestMinPoints = findBestMinPoints(points, bestEpsilon);
-            std::cout << "> Best minPoints: " << bestMinPoints << std::endl;
-
-            infoCSV["best_Epsilon"] = bestEpsilon;
-            infoCSV["best_MinPoints"] = bestMinPoints;
+        else if (line[0] == 0)
+        {
 
             partition += "(";
+            double ARI = 0;
+            computation(points, partition, infoCSV, ARI, best_Epsilon, best_MinPoints);
+            auto ending = std::chrono::system_clock::now();
+            timeToInt(start, starting);
+            timeToInt(end, ending);
 
-            determineCoreAndGroup(points, bestEpsilon, bestMinPoints);
-            determineBorderAndOutlier(points, bestEpsilon);
-            printResults(points, partition);
-
-            if (canCalculateARI(points)) {
-                int fileDefinedGroupCount = infoCSV["#Clusters"];
-                double ARI = calculateARI(points, fileDefinedGroupCount, infoCSV["#Trees"]);
-                //std::cout << "\n> Calculated ARI score (Adjusted Rand Index, better if close to 1) = " << ARI << std::endl;
-            }
-            fprintf(out,"DBSCAN;%f;%d;%d;%d;%d;%d;%s\n", bestEpsilon, bestMinPoints, infoCSV["#Trees"], infoCSV["#Leaves"], infoCSV["#Clusters"], infoCSV["Noise"], partition.c_str());
+            // Initialize the end time here
+            // Retrieve time after every caculation is done
+            // After, compute difference between start and end.
+            fprintf(out,"DBSCAN;%f;%d;%d;%d;%d;%d;%f;%s;%ld\n",best_Epsilon, best_MinPoints, infoCSV["#Trees"], infoCSV["#Leaves"], infoCSV["#Clusters"], infoCSV["Noise"], ARI, partition.c_str(), end-start);
             points.clear();
             partition = "";
         }
 
     }
     printf("\n");
-
-    double bestEpsilon = findBestEpsilon(points);
-    std::cout << "> Best epsilon: " << bestEpsilon << std::endl;
-
-    int bestMinPoints = findBestMinPoints(points, bestEpsilon);
-    std::cout << "> Best minPoints: " << bestMinPoints << std::endl;
-
-    infoCSV["best_Epsilon"] = bestEpsilon;
-    infoCSV["best_MinPoints"] = bestMinPoints;
-
-    determineCoreAndGroup(points, bestEpsilon, bestMinPoints);
-    determineBorderAndOutlier(points, bestEpsilon);
-
+    double ARI = 0;
     partition += "(";
-    printResults(points, partition);
-    //printf("\n nyokay %d", infoCSV["#Clusters"]);
 
-    if (canCalculateARI(points)) {
-       // if (isARIRequired()) {
-            //int userDefinedGroupCount = userDefinedGroupCountInput();
-            int fileDefinedGroupCount = infoCSV["#Clusters"];
-            double ARI = calculateARI(points, fileDefinedGroupCount, infoCSV["#Trees"]);
-            //std::cout << "\n> Calculated ARI score (Adjusted Rand Index, better if close to 1) = " << ARI << std::endl;
-        //}
-    }
-    fprintf(out,"DBSCAN;%f;%d;%d;%d;%d;%d;%s\n", bestEpsilon, bestMinPoints, infoCSV["#Trees"], infoCSV["#Leaves"], infoCSV["#Clusters"], infoCSV["Noise"], partition.c_str());
+    computation(points, partition, infoCSV, ARI, best_Epsilon, best_MinPoints);
+    auto ending = std::chrono::system_clock::now();
+    timeToInt(start, starting);
+    timeToInt(end, ending);
+    fprintf(out,"DBSCAN;%f;%d;%d;%d;%d;%d;%f;%s;%ld\n", best_Epsilon, best_MinPoints, infoCSV["#Trees"], infoCSV["#Leaves"], infoCSV["#Clusters"], infoCSV["Noise"], ARI, partition.c_str(), end-start);
     fclose(out);
     points.clear();
+
     return 0;
 }
